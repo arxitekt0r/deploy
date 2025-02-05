07ef0583eefdd01c9a89732af67b6c961162e1f4
Tigran Yeghyan <171566309+arxitekt0r@users.noreply.github.com>
2025-02-06 03:27:49 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+python -m venv venv
+venv\Scripts\activate
 # Install dependencies (if not already installed by Render)
 pip install -r requirements.txt
-
 # Run the FastAPI app with uvicorn
 uvicorn main:app --host 0.0.0.0 --port 10000
